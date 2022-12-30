a80419fa9f81df288114d82dfe14e7e2852c5849
Javier Viesca <viesca@gmail.com>
2022-12-30 12:25:14 +0100

    Link activado página actual

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
 window.onload = function () {
 
-    document.querySelector("#menu").addEventListener("click", muestraNav)
-
-
+    document.querySelector(".nav_menu").addEventListener("click", muestraNav);
+    let links = document.querySelector(".nav_links");
+    let menu = document.querySelector(".nav_menu");
+    let enlaces = document.querySelectorAll(".nav_links_links")
 
     function muestraNav() {
-        let links = document.querySelector("#links");
-        let menu = document.querySelector("#menu");
-
         if (links.getAttribute("class") == "nav_links abrir") {
             links.classList.remove("abrir");
             links.classList.add("cerrar");
@@ -23,11 +21,32 @@ window.onload = function () {
             menu.classList.remove("abrir");
             menu.classList.add("cerrar");
         }
-        
-
-        
     }
 
+    for(let enlace of enlaces){
+        enlace.addEventListener("click",decoraLink);
+    }
+
+    function decoraLink(){
+        //Resetea background y color de letra de todos los links
+        for(let enlace of enlaces){
+            enlace.style.backgroundColor = "black";
+            enlace.style.color = "white"
+        }
+
+        //Estilos a link seleccionado 
+        this.style.backgroundColor = "white";
+        this.style.color = "black";
+
+        //Cerrar menu navegación cuando se seleccione un link (Media Query activado)
+        links.classList.remove("abrir");
+        links.classList.add("cerrar");
+        menu.classList.remove("cerrar");
+        menu.classList.add("abrir");
+       
+    }
+
+   
 
 
 }
